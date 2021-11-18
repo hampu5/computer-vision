3f434bda7f72bcf2c0577577e9ff9b62ccc11202
hampu5 <halu1701@student.miun.se>
2021-11-18 20:40:04 +0100

    Use metrices, Blur and Sharpen work now.

--- assignment2/ass2.py
+++ assignment2/ass2.py
@@ -4,25 +4,17 @@ import numpy as np
 img = cv.imread('bird.jpg')
 img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-def kernel_avg(kernel):
-    a = kernel[0][0]
-    b = kernel[0][1]
-    c = kernel[0][2]
-    d = kernel[1][0]
-    e = kernel[1][1]
-    f = kernel[1][2]
-    g = kernel[2][0]
-    h = kernel[2][1]
-    i = kernel[2][2]
-    avg = np.int_(a/9+b/9+c/9+d/9+e/9+f/9+g/9+h/9+i/9)
-    return avg
 
-def kernel_vec_avg(kernel):
-    a = kernel[0]
-    b = kernel[1]
-    c = kernel[2]
-    avg = np.int_(a/3+b/3+c/3)
-    return avg
+def kernel_avg(frame):
+    kernel = np.array([
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ])
+    kernel = kernel * 1/9
+
+    result = np.sum(np.multiply(frame, kernel))
+    return result
 
 def filter_blur_slow(img):
     rows, cols = img.shape
@@ -31,26 +23,41 @@ def filter_blur_slow(img):
     while i < rows - 2:
         j = 0
         while j < cols - 2:
-            kernel = [
-                [img[i, j], img[i+1, j], img[i+2, j]],
-                [img[i, j+1], img[i+1, j+1], img[i+2, j+1]],
-                [img[i, j+2], img[i+1, j+2], img[i+2, j+2]]
-            ]
-            out_img[i, j] = kernel_avg(kernel)
+            frame = np.array([
+                [img[i, j], img[i, j+1], img[i, j+2]],
+                [img[i+1, j], img[i+1, j+1], img[i+1, j+2]],
+                [img[i+2, j], img[i+2, j+1], img[i+2, j+2]]
+            ])
+            out_img[i, j] = kernel_avg(frame)
             j = j + 1
         i = i + 1
     return out_img
 
-def filter_blur_fast_2step(img):
+
+def kernel_blur_vec(frame):
+    kernel = np.array([1, 1, 1])
+    kernel = kernel * 1/3
+
+    result = np.sum(np.multiply(frame, kernel))
+    return result
+
+def kernel_sharp_vec(frame):
+    kernel = np.array([1, 1, 1])
+    kernel = kernel * 1/3
+
+    result = np.sum(np.multiply(frame, kernel))
+    return frame[1] + 0.7 * (frame[1] - result)
+
+def separable_filter(img, kernel):
     rows, cols = img.shape
 
     out_col = np.zeros((rows - 2, cols), np.uint8)
     i = 0
     while i < rows - 2:
         j = 0
-        while j < cols - 2:
-            kernel_col = [img[i, j], img[i + 1, j], img[i + 2, j]]
-            out_col[i, j] = kernel_vec_avg(kernel_col)
+        while j < cols:
+            kernel_col = np.array([img[i, j], img[i + 1, j], img[i + 2, j]])
+            out_col[i, j] = kernel(kernel_col)
             j = j + 1
         i = i + 1
 
@@ -59,20 +66,21 @@ def filter_blur_fast_2step(img):
 
     out_row = np.zeros((rows, cols - 2), np.uint8)
     i = 0
-    while i < rows - 2:
+    while i < rows:
         j = 0
         while j < cols - 2:
             kernel_row = [out_col[i, j], out_col[i, j + 1], out_col[i, j + 2]]
-            out_row[i, j] = kernel_vec_avg(kernel_row)
+            out_row[i, j] = kernel(kernel_row)
             j = j + 1
         i = i + 1
 
     return out_row
 
+
 # out_img = filter_blur_slow(img)
-out_img = filter_blur_fast_2step(img)
+out_img = separable_filter(img, kernel_sharp_vec)
 
 
-# cv.imshow('Original', img)
-# cv.imshow('Blurred', out_img)
-# cv.waitKey(0)
+cv.imshow('Original', img)
+cv.imshow('Blurred', out_img)
+cv.waitKey(0)
