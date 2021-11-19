118f7c9c40c2a47b84638213487876a673adf696
hampu5 <halu1701@student.miun.se>
2021-11-19 22:18:50 +0100

    Minor changes

--- assignment3/ass3.py
+++ assignment3/ass3.py
@@ -6,25 +6,28 @@ point1a = [2, 2]
 point2a = [3, 1.15]
 point3a = [6, 0]
 
-numberArr = []
-for i in range(50):
-    numberArr.append(i)
-
-theta = np.array(numberArr)
-theta = theta * 2 * np.pi / theta.size
-
-def getDistance(point, theta):
-    return point[0] * np.cos(theta) + point[1] * np.sin(theta)
-
 # Plot the points
 plt.plot(point1a[0], point1a[1], marker='o')
 plt.plot(point2a[0], point2a[1], marker='o')
 plt.plot(point3a[0], point3a[1], marker='o')
 
-distance1a = getDistance(point1a, theta)
+# Create theta values
+theta = np.arange(0, 2 * np.pi, 2 * np.pi / 50)
+print(theta)
+
+def getDistance(point, theta):
+    return point[0] * np.cos(theta) + point[1] * np.sin(theta)
+
 distance2a = getDistance(point2a, theta)
+distance1a = getDistance(point1a, theta)
 distance3a = getDistance(point3a, theta)
 
+# Plot the sine curves
+plt.plot(theta, distance1a)
+plt.plot(theta, distance2a)
+plt.plot(theta, distance3a)
+
+# Get the point where the curves intersect
 diffCross = 100
 outTheta = 0
 outR = 0
@@ -35,17 +38,10 @@ for i in range(10):
     if diff < diffCross and min > 0:
         print(diff)
         diffCross = diff
-        outR = min + diff / 2
         outTheta = i * 2 * np.pi / theta.size
+        outR = min + diff / 2
 
 crossPoint = [outTheta, outR]
-
-
-
-# Plot the sine curves
-plt.plot(theta, distance1a)
-plt.plot(theta, distance2a)
-plt.plot(theta, distance3a)
 
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
