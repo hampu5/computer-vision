3650b6c17ba65fa3ab27608ae7d92e036f8ff1f2
hampu5 <halu1701@student.miun.se>
2021-12-12 15:24:03 +0100

    I think I am satisfied with plane sweep.

--- assignment4/ass4-3.py
+++ assignment4/ass4-3.py
@@ -37,26 +37,83 @@ def plane_sweep2(img0, img1):
     disparity = 0
     
     while disparity < cols:
+
         i = 0
         while i < rows:
             min_diff = np.inf
             true_disp = 0
+
             j = disparity
             while j < cols: # - disparity:
-                diff = img1[i, j] - img0[i, disparity]
+                diff = img1[i, disparity] - img0[i, j]
+
                 if diff < min_diff:
                     min_diff = diff
-                    print(diff)
+                    # print(diff)
                     true_disp = j - disparity # or -k
+                
                 j += 1
+            
             out_img[i, disparity] = true_disp
             i += 1
+        
         disparity += 1
         # print(disparity)
+    
     out_img = out_img / np.amax(out_img)
     return out_img
 
+def compute_window(img0, img1, i, j, shift):
+    window_size = 5
+    
+    mat_row = []
+    start_index = -int(np.floor(window_size / 2))
+    row = start_index
+    while row < window_size + start_index:
+        mat_col = []
+        col = 0
+        while col < window_size + start_index:
+            if i+row < 0 or i+row >= 274 or j+col < 0 or j+col >= 400 or j+shift+col < 0 or j+shift+col >= 400:
+                calculation = 0
+            else:
+                calculation = np.abs(img0[i + row, j + shift + col] - img1[i + row, j + col])
+            mat_col.append(calculation)
+            col += 1
+        mat_row.append(mat_col)
+        row += 1
+    
+    diff = np.sum(mat_row) / (window_size**2)
+    return diff
+        
 
+def plane_sweep3(img0, img1):
+    rows, cols = img0.shape
+    diff_img = np.full((rows, cols), 255, np.float32)
+    out_img = np.zeros((rows, cols), np.float32)
+    img0 = np.array(img0, np.float32)
+    img1 = np.array(img1, np.float32)
+
+    block_size = 30
+    for shift in range(0, cols, 5):
+        if shift > block_size:
+            break
+        for i, rows in enumerate(diff_img):
+            for j, elem in enumerate(rows):
+                # if j < shift:
+                #     continue
+                if j + shift >= cols:
+                    break
+                # diff = np.abs(img0[i, j + shift] - img1[i, j])
+                diff = compute_window(img0, img1, i, j, shift)
+                # print(diff)
+                if diff < elem:
+                    diff_img[i, j] = diff
+                    # print(shift/block_size)
+                    out_img[i, j] = shift / block_size
+    
+    # out_img = out_img / np.amax(out_img)
+    return out_img
+            
 
 
 def stereo_match(img0, img1):
@@ -103,7 +160,7 @@ def stereo_match(img0, img1):
     img_d = img_d / np.amax(img_d)
     return img_d
 
-img_d = plane_sweep2(img0, img1)
+img_d = plane_sweep3(img0, img1)
 
 cv.imshow("Disparity image", img_d)
 
