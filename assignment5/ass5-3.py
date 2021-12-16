7f6fd5209cd9ec039444bd5c9b68fd7961c9674c
hampu5 <halu1701@student.miun.se>
2021-12-16 23:00:24 +0100

    WOrking nice k-means now

--- assignment5/ass5-3.py
+++ assignment5/ass5-3.py
@@ -7,31 +7,32 @@ img = cv.imread('pic2_small.jpg')
 row, col, color = img.shape
 
 # Creating an extended image with x and y parameters as well
-img_extended = np.zeros((row, col, 5))
-for x in range(row):
-    for y in range(col):
-        img_extended[x, y, 0] = img[x, y, 0]
-        img_extended[x, y, 1] = img[x, y, 1]
-        img_extended[x, y, 2] = img[x, y, 2]
-        img_extended[x, y, 3] = x
-        img_extended[x, y, 4] = y
+# img_extended = np.zeros((row, col, color))
+# for x in range(row):
+#     for y in range(col):
+#         img_extended[x, y, 0] = img[x, y, 0]
+#         img_extended[x, y, 1] = img[x, y, 1]
+#         img_extended[x, y, 2] = img[x, y, 2]
+#         img_extended[x, y, 3] = x
+#         img_extended[x, y, 4] = y
+
 
 # Reshaping before putting into kmeans()
-img_extended = np.reshape(img_extended, (-1, 5))
-img = np.reshape(img, (-1, 5))
+img = np.reshape(img, (-1, color))
 
-clusters = km.kmeans(img_extended, [[20, 15 , 8, 350, 200], [200, 70, 50, 60, 230], [200, 200, 200, 350, 350]], 10)
+centroids = [
+    [20, 15 , 8],
+    [200, 70, 50],
+    [200, 200, 200]]
 
-# out_img = np.reshape(img_extended, (row, col, color))
+labels, centers = km.kmeans(img, centroids, 5)
+
+# Recoloring the image from the labels, using the centers
+for i, (pixel, label) in enumerate(zip(img, labels)):
+    img[i] = centers[label]
+
+# Reshaping the image again before showing
 img = np.reshape(img, (row, col, color))
-
-# From clusters, add points to the correct pixel places using their x and y parameters
-for cluster in clusters:
-    print(cluster[0])
-    for point in cluster[1]:
-        c = [cluster[0][0], cluster[0][1], cluster[0][2]]
-        img[int(point[3]), int(point[4])] = c
-
 
 cv.imshow('Image', img)
 cv.waitKey(0)
--- assignment5/kmeans.py
+++ assignment5/kmeans.py
@@ -1,79 +1,73 @@
 import numpy as np
+import copy
 
-x1 = [0, 0.5]
-x2 = [0, 0.76]
-x3 = [1, 1]
-x4 = [1.2, 0.4]
-x5 = [1.5, 0.75]
-x6 = [2.5, 1]
-x7 = [3, 2]
-x8 = [4, 1.5]
-x9 = [4, 2.5]
-x10 = [5, 2]
+# x1 = np.array([0, 0.5])
+# x2 = np.array([0, 0.76])
+# x3 = np.array([1, 1])
+# x4 = np.array([1.2, 0.4])
+# x5 = np.array([1.5, 0.75])
+# x6 = np.array([2.5, 1])
+# x7 = np.array([3, 2])
+# x8 = np.array([4, 1.5])
+# x9 = np.array([4, 2.5])
+# x10 = np.array([5, 2])
 
-m1 = [1, 1.5]
-m2 = [3, 1]
-
-cluster_m1 = []
-cluster_m2 = []
+# m1 = np.array([1, 1.5])
+# m2 = np.array([3, 1])
 
 
-def kmeans(points: list, centroids: list, iterations: int):
+def kmeans(points: np.ndarray, centroids: np.ndarray, iterations: int):
     k = len(centroids)
-    dim = len(centroids[0])
 
-    clusters = []
-    for centroid in centroids:
-        clusters.append([centroid, []])
+    c = copy.deepcopy(centroids)
+    c_size = np.zeros(k)
 
+    labels = np.zeros(len(points), np.uint16)
+    
     def assign_points():
-        for i, centroid in enumerate(clusters):
-            clusters[i][1] = []
-            # print(clusters[i])
-        
-        for point in points:
-            summed_squares = []
-            for centroid, cluster in clusters:
-                # Summed Square Difference of the coordinate components
-                summed_square = 0
-                for point_coord, centroid_coord in zip(point, centroid):
-                    summed_square += (point_coord - centroid_coord)**2
-                summed_squares.append(summed_square)
+        # Loop through all the points
+        for p_i, point in enumerate(points):
+            # Find the summed squared distances from the point
+            # to the different centroids
+            ssd_p_to_c = []
+            for centroid in c:
+                # Summed Square Difference
+                ssd = np.sum(np.square(np.subtract(point, centroid)))
+                ssd_p_to_c.append(ssd)
             
-            # Find the index for the cluster center closest to the point
-            min_ss = np.inf
-            index = 0
-            for i, ss in enumerate(summed_squares):
-                if ss < min_ss:
-                    min_ss = ss
-                    index = i
-            clusters[index][1].append(point)
+            # Find the index of the closest centroid
+            closest_c_i = np.argmin(ssd_p_to_c)
 
-        return clusters
+            # Set the label for this point (p_i) to the index
+            # of the closest centroid (closest_c_i)
+            labels[p_i] = closest_c_i
+
+            # Increment the size of the cluster with the closest centroid
+            # by 1 (That is, count this point as member of that cluster)
+            c_size[closest_c_i] += 1
         
     def calculate_centroids():
-        for cluster in clusters:
-            size = len(cluster[1])
+        # Set all centroids to 0
+        for c_i, centroid in enumerate(c):
+            c[c_i] = [0 for x in centroid]
 
-            # Set all cluster centers to 0
-            cluster[0] = []
-            for i in range(dim):
-                cluster[0].append(0)
-
-            # Compute new cluster centers from the
-            # points assigned to each cluster
-            for point in cluster[1]:
-                for i in range(dim):
-                    cluster[0][i] += point[i] / size
+        # Compute new cluster centroids
+        for label, point in zip(labels, points):
+            # Get the size of the cluster with index: label
+            size = c_size[label]
+            
+            # Set the new centroids
+            c[label] += point / size
     
     i = 0
     while i < iterations:
         assign_points()
         calculate_centroids()
+        c_size = [0 for x in c_size]
         i += 1
     
-    return clusters
+    return labels, c
 
-# clusters = kmeans([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10], [m1, m2], 2)
+# labels, centers = kmeans(np.array([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]), np.array([m1, m2]), 2)
 
-# print(clusters)
+# print(centers)
