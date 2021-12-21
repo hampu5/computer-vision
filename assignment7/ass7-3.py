c3e68eb238a48a63caef26f9f41e91663a8d91ff
hampu5 <halu1701@student.miun.se>
2021-12-21 16:34:17 +0100

    Shaders are working now.

--- assignment7/ass7-3.py
+++ assignment7/ass7-3.py
@@ -4,56 +4,42 @@ import ratcave as rc
 
 # Shaders
 vert_shader = """
-#version 130
 attribute vec4 vertexPosition;
-uniform mat4 projection_matrix, view_matrix, model_matrix;
+attribute vec4 normalPosition;
+uniform mat4 projection_matrix, view_matrix, model_matrix, normal_matrix;
 
-out vec4 FragPos;
-// out vec3 Normal;
-// vec3 aPos = gl_Vertex.xyz;
-// vec3 aNormal = gl_Normal;
+varying vec3 FragPos;
+varying vec3 Normal;
 
 void main()
 {
     gl_Position = projection_matrix * view_matrix * model_matrix * vertexPosition;
-    // FragPos = vec3(model_matrix * vec4(gl_Position.xyz, 1.0));
-    FragPos = gl_Position;
-    // Normal = aNormal;
+    FragPos = vec3(model_matrix * vertexPosition);
+    Normal = normalize(normal_matrix * normalPosition).xyz;
 }
 """
 
 frag_shader = """
-#version 130
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 
-in vec4 FragPos;
-// in vec3 Normal;
-
 uniform vec3 lightPos;
+
+varying vec3 FragPos;
+varying vec3 Normal;
 
 void main()
 {
     // Ambient light
-    float ambientStrength = 1;
+    float ambientStrength = 0.3;
     vec3 ambient = ambientStrength * lightColor;
     
     // Diffuse light (Lambertian)
-    
-    vec3  ndc_pos = FragPos.xyz / FragPos.w;
-    vec3  dx      = dFdx( ndc_pos );
-    vec3  dy      = dFdy( ndc_pos );
-
-    vec3 Normal = normalize(cross(dx, dy));
-    Normal *= sign(Normal.z);
-
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos.xyz);
-    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(Normal, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
-    //vec3 result = (ambient + diffuse) * objectColor;
-    vec3 result = (ambient) * objectColor;
+    vec3 result = (ambient + diffuse) * objectColor;
     gl_FragColor = vec4(result, 1.0);
 }
 """
@@ -79,12 +65,10 @@ monkey.position.xyz = 0, 0, -2
 
 monkey.uniforms['objectColor'] = [.2, .3, .5]
 monkey.uniforms['lightColor'] = [.8, .8, .1]
-monkey.uniforms['lightPos'] = [0, 0, 3]
+monkey.uniforms['lightPos'] = [.0, .0, 3.]
 
 # Create Scene
 scene = rc.Scene(meshes=[monkey])
-
-scene.light = rc.Light
 
 def move_camera(dt):
     camera_speed = 3
